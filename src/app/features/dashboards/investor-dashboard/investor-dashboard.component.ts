bdc56beef1f8e552ee726e688e7e961ba44ecfbf
Nickodee <nicodemusmuholo@gmail.com>
2025-08-27 15:54:54 +0300

    Added routing

---
+++ src/app/features/dashboards/investor-dashboard/investor-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investor-dashboard',
+  imports: [],
+  templateUrl: './investor-dashboard.component.html',
+  styleUrl: './investor-dashboard.component.css'
+})
+export class InvestorDashboardComponent {
+
+}
